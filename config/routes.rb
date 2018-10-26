16cc5581fd2b9eeebf3ab8f393bbf57b43900dd0
Roman Belik <dabonka@yandex.ru>
2018-10-26 09:50:15 +0300

    add controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  mount_devise_token_auth_for 'User', at: '/api/v1/auth'
+  namespace :api do
+    namespace :v1, defaults: { format: 'json' } do
+      resources :apidocs, only: [:index]
+      devise_scope :user do
+        put 'set_password', to: 'passwords#set_new_password'
+      end
+      resources :users do
+        resources :filters
+      end
+    end
+  end
 end
